bde1eef85f5b53a36ba0b3a9d66cdb47e70e835e
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-05-05 19:11:07 -0400

    Subindo servico de autenticacao, authService, jsonWebToken lib, fechando rotas

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,6 +4,7 @@ const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/custormer-repository');
 const md5 = require('md5');
 const emailService = require('../services/email-service');
+const authService = require('../services/auth-service');
 
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
@@ -24,11 +25,47 @@ exports.post = async (req, res, next) => {
         });
 
         emailService.send(
-            req.body.email, 
-            'Bem vindo ao node store', 
+            req.body.email,
+            'Bem vindo ao node store',
             global.EMAIL_TMPL.replace('{0}', req.body.name));
 
         res.status(201).send({ message: 'Cliente cadastrado com sucesso!' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+};
+
+
+exports.authenticate = async (req, res, next) => {
+
+    try {
+        const customer = await repository.authenticate({
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
+
+        if(!customer){
+            res.status(404).send({
+                message: 'Usuário ou senha inválidos'
+            });
+            return;
+        }
+
+        const token = await authService.generateToken({
+            email: customer.email,
+            name: customer.name
+        });
+
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
     } catch (e) {
         res.status(500).send({
             message: 'Falha ao processar sua requisição'
--- src/repositories/custormer-repository.js
+++ src/repositories/custormer-repository.js
@@ -16,3 +16,11 @@ exports.create = async (data) => {
     await customer.save();
 }
 
+exports.authenticate = async (data) => {
+    const res = await Customer
+        .findOne({
+            email: data.email,
+            password: data.password
+        });
+    return res;
+}
