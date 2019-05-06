9708bd8c67b8a822cf948d0595f15bcc36e34f3a
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-05-05 20:34:47 -0400

    subindo validacoes de roles no authService

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -21,7 +21,8 @@ exports.post = async (req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ["user"]
         });
 
         emailService.send(
@@ -56,7 +57,8 @@ exports.authenticate = async (req, res, next) => {
         const token = await authService.generateToken({
             id: customer._id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
 
@@ -96,7 +98,8 @@ exports.refreshToken = async (req, res, next) => {
         const tokenGerado = await authService.generateToken({
             id: customer._id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
 
