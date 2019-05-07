74979c7d872e52c61a26e03540d36ae51cf8440c
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-05-06 20:13:04 -0400

    Adiconando permissões no CORS e limit de json

--- src/app.js
+++ src/app.js
@@ -21,10 +21,20 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb' //limite de um corpo json
+}));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //libera todas as urls
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token'); //adicionado o token
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); //adicionando os metodos http
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
